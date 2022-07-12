61d9328578f8308ab520ae8c7f22c3d7ce98fea3
Hong Minhee <hong.minhee@gmail.com>
2022-07-12 15:05:46 +0900

    Initial commit

---
+++ Casks/intellij-idea-ce-eap.rb
@@ -0,0 +1,52 @@
+cask "intellij-idea-ce-eap" do
+  arch = Hardware::CPU.intel? ? "" : "-aarch64"
+
+  version "2022.2,222.3345.16"
+
+  if Hardware::CPU.intel?
+    sha256 "00e212d51f6cb12cfb639ffeb3afe5419db9e9f07b92b24672187d44e6079b58"
+  else
+    sha256 "e1b46ffc368c81e5b0e27f7f9bc76a3526957084231adafa761045486f5d0eae"
+  end
+
+  url "https://download.jetbrains.com/idea/ideaIC-#{version.after_comma}#{arch}.dmg"
+  name "IntelliJ IDEA Community Edition EAP"
+  name "IntelliJ IDEA CE EAP"
+  desc "IDE for Java development - community edition (EAP)"
+  homepage "https://www.jetbrains.com/idea/nextversion/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=IIC&latest=true&type=eap"
+    strategy :page_match do |page|
+      JSON.parse(page)["IIC"].map do |release|
+        "#{release["version"]},#{release["build"]}"
+      end
+    end
+  end
+
+  auto_updates true
+  conflicts_with cask: [
+    "homebrew/cask/intellij-idea-ce",
+    "homebrew/cask-versions/intellij-idea-ce19",
+  ]
+  depends_on macos: ">= :high_sierra"
+
+  app "IntelliJ IDEA #{version.major_minor} CE EAP.app"
+
+  uninstall_postflight do
+    ENV["PATH"].split(File::PATH_SEPARATOR).map { |path| File.join(path, "idea") }.each do |path|
+      if File.readable?(path) &&
+         File.readlines(path).grep(/# see com.intellij.idea.SocketLock for the server side of this interface/).any?
+        File.delete(path)
+      end
+    end
+  end
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/IdeaIC#{version.major_minor}",
+    "~/Library/Caches/JetBrains/IdeaIC#{version.major_minor}",
+    "~/Library/Logs/JetBrains/IdeaIC#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.intellij.ce-EAP.plist",
+    "~/Library/Saved Application State/com.jetbrains.intellij.ce-EAP.savedState",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+JetBrains EAP Casks for Homebrew Cask
+=====================================
+
+Since [homebrew-cask and homebrew-cask-versions decided to do not accept all
+JetBrains EAP versions][1], here we maintain a separated tap for them.
+
+    brew tap dahlia/jetbrains-eap
+    brew install intellij-idea-ce-eap
+
+[1]: https://github.com/Homebrew/homebrew-cask/issues/32521
