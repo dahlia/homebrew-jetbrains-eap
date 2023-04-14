26cd64e7a0aa0adf5ac2360b9bd801ea0b96c1dd
HongBot <109153616+hong-bot@users.noreply.github.com>
2023-04-14 11:07:33 +0000

    rubymine-eap 2023.1.1

--- Casks/rubymine-eap.rb
+++ Casks/rubymine-eap.rb
@@ -1,9 +1,9 @@
 cask "rubymine-eap" do
   arch arm: "-aarch64"
 
-  version "2023.1,231.8109.125"
-  sha256 arm:   "d87ee0f3ea48e3353efef844c3e17714a0e8395de64077c353456ccbe6acb832",
-         intel: "5852d32909705c3e380d20a2d7edee62bfda996c742acb901f6324e81341bc5a"
+  version "2023.1.1,231.8770.5"
+  sha256 arm:   "d26b586daa6c46efda295859793faf005147a74fe1145886a4e7d868a2cb37f0",
+         intel: "f6e988504013b84bba2c3e623d479d5aaf2c529f69d6b03d9566807e92020bdf"
 
   url "https://download.jetbrains.com/ruby/RubyMine-#{version.csv.second}#{arch}.dmg"
   name "RubyMine EAP"
