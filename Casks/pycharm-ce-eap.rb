8bdc53832c37f18a0218eba8ef4d7e498279f2d1
HongBot <109153616+hong-bot@users.noreply.github.com>
2023-10-06 14:06:45 +0000

    pycharm-ce-eap 2023.3,233.9102.128

--- Casks/pycharm-ce-eap.rb
+++ Casks/pycharm-ce-eap.rb
@@ -1,9 +1,9 @@
 cask "pycharm-ce-eap" do
   arch arm: "-aarch64"
 
-  version "2023.3,233.6745.319"
-  sha256 intel: "5d56fc09cf6695b505aa0dac3e3b8880d5ac4d6ca68476c651536a4984c8625c",
-         arm:   "7303eb6f72c2ee2e6c0cc56c2e898fb5235f811ba8593622c81b104a665b33b5"
+  version "2023.3,233.9102.128"
+  sha256 intel: "01a701d41b003e9b6b2123f40e80bc062d997f88cf88278f0b558c38ba247791",
+         arm:   "3efc83c20b1297f0b3f8c73b19ba36fdd066f0a3dd3903428121bb4dc5050c56"
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version.csv.second}#{arch}.dmg"
   name "Jetbrains PyCharm Community Edition EAP"
