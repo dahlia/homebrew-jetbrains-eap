f0d9cc1dd74ec12a93b5304fc4057af20fc5edbf
HongBot <109153616+hong-bot@users.noreply.github.com>
2023-11-03 17:06:28 +0000

    datagrip-eap 2023.3,233.11555.17

--- Casks/datagrip-eap.rb
+++ Casks/datagrip-eap.rb
@@ -1,9 +1,9 @@
 cask "datagrip-eap" do
   arch arm: "-aarch64"
 
-  version "2023.3,233.10527.14"
-  sha256 intel: "31096f2f23e1493b0433318f83d40a273960a7be2bbd4eeacd92f6a3beac2927",
-         arm:   "d1b53b4d14eb7e147d2a4a53dd02da4444ddcf8cda646859aae29916e542b34b"
+  version "2023.3,233.11555.17"
+  sha256 intel: "56d00ff22375844cf29ab58b2dfdc489760d2cb07ea6d62f65d8ddfdb8c04f54",
+         arm:   "6acb52a74564ba9cc3816c5ecec2e2be5e2539f23ddbef3e93f695270be59d93"
 
   url "https://download.jetbrains.com/datagrip/datagrip-#{version.csv.second}#{arch}.dmg"
   name "DataGrip EAP"
