b3b81a5e4add8705cec266187f576bb465a92a0d
HongBot <109153616+hong-bot@users.noreply.github.com>
2023-11-04 15:21:14 +0900

    intellij-idea-ce-eap 2023.3,233.11555.11 (#355)

--- Casks/intellij-idea-ce-eap.rb
+++ Casks/intellij-idea-ce-eap.rb
@@ -1,9 +1,9 @@
 cask "intellij-idea-ce-eap" do
   arch arm: "-aarch64"
 
-  version "2023.3,233.11361.10"
-  sha256 arm:   "a36d357c4eeb229683f09a17ddea6601e3b1f11c01fb6b6203ffa507f92f7fd1",
-         intel: "d989bfbeeb1d56b8b40a34e5962eef0b7e5204435ff201e25715941788604eb2"
+  version "2023.3,233.11555.11"
+  sha256 arm:   "2ced8b421d8957038b3db5b9b1d6115e05f4f92b5e7018626ff5c03ca62bb06c",
+         intel: "ba384d3118d242e059c2f27af96acc9dfb8256097f15f265dc2a988b84d2019d"
 
   url "https://download.jetbrains.com/idea/ideaIC-#{version.csv.second}#{arch}.dmg"
   name "IntelliJ IDEA Community Edition EAP"
