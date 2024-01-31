5d340ec25a1488f54ef4516d250094ac2e8d6531
Hong Minhee <hong@minhee.org>
2024-01-31 10:48:51 +0900

    webstorm-eap 2024.1,241.9959.35

--- Casks/webstorm-eap.rb
+++ Casks/webstorm-eap.rb
@@ -1,11 +1,11 @@
 cask "webstorm-eap" do
   arch arm: "-aarch64"
 
-  version "2024.1,241.8651"
-  sha256 intel: "d947caa4ea4297e7371ffe9d8f217f17f27df9b6b09a334ba4fa5b67d5e0a16d",
-         arm:   "85c29f7f3ebdac451a9f5018fc6cf2e6b8fdc91662766228fa918b3f7badb0d4"
+  version "2024.1,241.9959.35"
+  sha256 intel: "b3022c25d43c69568b8e7b08462ffea7e2ae432e17ce181985a7fad199274c6d",
+         arm:   "1c4d3557e921206eaaccc54ddd85bef399a7feb3a77de42ac8dd414462c8e0e1"
 
-  url "https://download.jetbrains.com/webstorm/nightly/WebStorm-#{version.csv.second}#{arch}.dmg"
+  url "https://download.jetbrains.com/webstorm/WebStorm-#{version.csv.second}#{arch}.dmg"
   name "WebStorm EAP"
   desc "JavaScript IDE (EAP)"
   homepage "https://www.jetbrains.com/webstorm/nextversion/"
