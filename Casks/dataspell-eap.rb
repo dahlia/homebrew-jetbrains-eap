e006720c1053946e7db911421e6acaa1003d8c05
HongBot <109153616+hong-bot@users.noreply.github.com>
2024-03-18 17:08:29 +0000

    dataspell-eap 2024.1,241.14494.121

--- Casks/dataspell-eap.rb
+++ Casks/dataspell-eap.rb
@@ -1,9 +1,9 @@
 cask "dataspell-eap" do
   arch arm: "-aarch64"
 
-  version "2024.1,241.10840.15"
-  sha256 intel: "54223e6f1e0c97f266a768bfaec7c642bc1655b16077f65c2d8a6e67af6f42cf",
-         arm:   "da1865713af4217daca8ae0526d4380aec683b1a66625b1ef2b313c38f5b9a41"
+  version "2024.1,241.14494.121"
+  sha256 intel: "00aa8da261a9e5d4288d39de318d4b54224a4bb18bc4f9b72b08b3a97b5d6ab8",
+         arm:   "a2da2e47dbabff174c59912a758468beeec16e41ca3ebde22be30361c0d81ce2"
 
   url "https://download.jetbrains.com/python/dataspell-#{version.csv.second}#{arch}.dmg"
   name "DataSpell EAP"
