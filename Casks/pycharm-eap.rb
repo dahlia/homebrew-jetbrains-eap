788ff2c44ac3c116bd24eaa26a82a55fc5e796ce
HongBot <109153616+hong-bot@users.noreply.github.com>
2025-05-23 15:09:07 +0000

    pycharm-eap 2025.2

--- Casks/pycharm-eap.rb
+++ Casks/pycharm-eap.rb
@@ -1,9 +1,9 @@
 cask "pycharm-eap" do
   arch arm: "-aarch64"
 
-  version "2025.1,251.23774.354"
-  sha256 intel: "bca82cf215be52ba9892dfdd5edd4c2da158c8afd1060a92375ada74dce82e04",
-         arm:   "10c616a426b4207f67a4ce5c303c8ab72f17c52efa9aa0b527abe82b3b6b3574"
+  version "2025.2,252.16512.37"
+  sha256 intel: "0571deec35e6367a79df73fb6a14e4614632c1d67a32e103649f7bcd2c5a66ff",
+         arm:   "4d5e77458926524c04c5d89eff07945fb8ac6767592e0d1ce9a2aaef22e2747e"
 
   url "https://download.jetbrains.com/python/pycharm-professional-#{version.csv.second}#{arch}.dmg"
   name "PyCharm EAP"
