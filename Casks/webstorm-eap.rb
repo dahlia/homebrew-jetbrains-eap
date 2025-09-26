bc16a345843198ac21823e90f0d45809e47f20a9
HongBot <109153616+hong-bot@users.noreply.github.com>
2025-09-26 12:34:16 +0900

    webstorm-eap 2025.3,253.22441.34 (#898)

--- Casks/webstorm-eap.rb
+++ Casks/webstorm-eap.rb
@@ -1,9 +1,9 @@
 cask "webstorm-eap" do
   arch arm: "-aarch64"
 
-  version "2025.3,253.20558.45"
-  sha256 intel: "e7400de66aef86c969319bfc31380f84465e18ab55de7ba12d10078402b18f7c",
-         arm:   "c2581bde844e3966285f04ef415f3a713e6e53eae939fe61af38c35e08797ec3"
+  version "2025.3,253.22441.34"
+  sha256 intel: "a91f08136ec876c72b2c8dc9041f05c675183a76cb09ade0c37b2f1a42a5611d",
+         arm:   "a862f45a4ac4d4f020f08c8ebce15288194e2b5aa1f5d67c4e9f1ab7f7dfe750"
 
   url "https://download.jetbrains.com/webstorm/WebStorm-#{version.csv.second}#{arch}.dmg"
   name "WebStorm EAP"
