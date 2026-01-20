ce9e5c5ba98b061cad4987073759f3821a672a6f
Goooler <wangzongler@gmail.com>
2026-01-20 10:05:10 +0800

    Update intellij-idea-eap from intellij-idea@eap
    https://github.com/Homebrew/homebrew-cask/blob/e126662a5414857b0a8c94e483e468fde033ea54/Casks/i/intellij-idea%40eap.rb

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -6,23 +6,30 @@ cask "intellij-idea-eap" do
          intel: "ebb3f6885a546c91e8ac5aa204327c4ad31120beff077b8170c07c8455b00eb3"
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version.csv.second}#{arch}.dmg"
-  name "IntelliJ IDEA Ultimate"
-  desc "Java IDE by JetBrains"
-  homepage "https://www.jetbrains.com/idea/nextversion/"
+  name "IntelliJ IDEA EAP"
+  desc "IntelliJ IDEA Early Access Program"
+  homepage "https://www.jetbrains.com/idea/nextversion"
 
   livecheck do
-    url "https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=eap"
-    strategy :page_match do |page|
-      JSON.parse(page)["IIU"].map do |release|
-        "#{release["version"]},#{release["build"]}"
+    url "https://data.services.jetbrains.com/products/releases?code=IIU&release.type=eap"
+    strategy :json do |json|
+      json["IIU"]&.map do |release|
+        version = release["version"]
+        build = release["build"]
+        next if version.blank? || build.blank?
+
+        "#{version},#{build}"
       end
     end
   end
 
   auto_updates true
 
-  app "IntelliJ IDEA #{version.major_minor} EAP.app"
-  binary "#{appdir}/IntelliJ IDEA #{version.major_minor} EAP.app/Contents/MacOS/idea", target: "idea-eap"
+  # The application path is often inconsistent between versions
+  rename "IntelliJ IDEA*.app", "IntelliJ IDEA EAP.app"
+
+  app "IntelliJ IDEA EAP.app"
+  binary "#{appdir}/IntelliJ IDEA EAP.app/Contents/MacOS/idea", target: "idea-eap"
 
   uninstall_postflight do
     ENV["PATH"].split(File::PATH_SEPARATOR).map { |path| File.join(path, "idea") }.each do |path|
@@ -34,10 +41,12 @@ cask "intellij-idea-eap" do
   end
 
   zap trash: [
-    "~/Library/Application Support/JetBrains/IntelliJIdea#{version.major_minor}",
-    "~/Library/Caches/JetBrains/IntelliJIdea#{version.major_minor}",
-    "~/Library/Logs/JetBrains/IntelliJIdea#{version.major_minor}",
-    "~/Library/Preferences/com.jetbrains.intellij-EAP.plist",
-    "~/Library/Saved Application State/com.jetbrains.intellij-EAP.savedState",
+    "~/Library/Application Support/JetBrains/IntelliJIdea#{version.csv.first}",
+    "~/Library/Caches/JetBrains/IntelliJIdea#{version.csv.first}",
+    "~/Library/Logs/JetBrains/IntelliJIdea#{version.csv.first}",
+    "~/Library/Preferences/com.jetbrains.intellij.plist",
+    "~/Library/Preferences/IntelliJIdea#{version.csv.first}",
+    "~/Library/Preferences/jetbrains.idea.*.plist",
+    "~/Library/Saved Application State/com.jetbrains.intellij.savedState",
   ]
 end
