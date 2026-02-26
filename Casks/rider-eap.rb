7f116932496db261016ad0d9dd446ca6ec0e0c64
HongBot <109153616+hong-bot@users.noreply.github.com>
2026-02-26 15:21:12 +0000

    rider-eap 2026.1-EAP6

--- Casks/rider-eap.rb
+++ Casks/rider-eap.rb
@@ -1,9 +1,9 @@
 cask "rider-eap" do
   arch arm: "-aarch64"
 
-  version "2026.1-EAP5,261.21525.32"
-  sha256 arm:   "db12eeb9454b7bb08f5ebd9536f8fcfde0e3db20a784ba33a19d79b97b2a2664",
-         intel: "78b6f0d4769148fe70ad55f2d6453ca5c0d5f95a3c1ebfb923251ee243e75f1b"
+  version "2026.1-EAP6,261.21849.29"
+  sha256 arm:   "dcc144b47e0554368aa4fc944d6b23f823de85f78a6773417eb540e68de56a59",
+         intel: "2a25b81811e5d465a2c487afee35933b8dcdcb500f75c473471059bf7f694d5f"
 
   url "https://download-cdn.jetbrains.com/rider/JetBrains.Rider-#{version.csv.first}-#{version.csv.second}.Checked#{arch}.dmg"
   name "JetBrains Rider EAP"
