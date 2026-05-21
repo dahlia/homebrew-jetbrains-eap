5ff5f1eebeda27d7df78822c6b53952f2d197862
HongBot <109153616+hong-bot@users.noreply.github.com>
2026-05-21 13:07:02 +0000

    phpstorm-eap 2026.2,262.6228.31

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -1,9 +1,9 @@
 cask "phpstorm-eap" do
   arch arm: "-aarch64"
 
-  version "2026.2,262.5752.40"
-  sha256 arm:   "d831dad90f9be97a3f91b9340aadfe2c0456e06cfc8421d4c8c01afa253119d6",
-         intel: "084f05370a47d7e5c3458ea60719c6a3ce012702d0e2e993f221697255134ba5"
+  version "2026.2,262.6228.31"
+  sha256 arm:   "b5ba8eb7c9bf32cace49a3b894d19f740e4d0b5434f0163d80f9cbaa676051c5",
+         intel: "74460e57746cabe63866639d402c2fe0fa5e87d0565f248800b1fd1c6cde5487"
 
   url "https://download.jetbrains.com/webide/PhpStorm-#{version.csv.second}#{arch}.dmg"
   name "JetBrains PhpStorm EAP"
