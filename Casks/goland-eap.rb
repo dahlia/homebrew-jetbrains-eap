ad552ff6c7ac00612c0ac7a18128b2ec3bd34a7b
HongBot <109153616+hong-bot@users.noreply.github.com>
2026-09-25 04:07:55 +0000

    goland-eap 2026.3,263.5153.37

--- Casks/goland-eap.rb
+++ Casks/goland-eap.rb
@@ -1,9 +1,9 @@
 cask "goland-eap" do
   arch arm: "-aarch64"
 
-  version "2026.3,263.3889.70"
-  sha256 arm:   "0d6537455971aafc0adb351eb1eba17283110d029ee7c69801fdb4a565e25dcb",
-         intel: "3c20b63df9d0082093dde970fe9e64be6c4334eee938469a40aaa03c01af83aa"
+  version "2026.3,263.5153.37"
+  sha256 arm:   "f09fa6f59b2f5c2c258384102ea85b5e0c16359d46df8762e6390a2b7824b65d",
+         intel: "153d5c4be3979ded9a7b718f4111ea340caf93923b222017b21e598ba7068a1f"
 
   url "https://download.jetbrains.com/go/goland-#{version.csv.second}#{arch}.dmg"
   name "GoLand EAP"
